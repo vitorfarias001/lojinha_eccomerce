bd37b1797252b82926637da86d12fec72d7703fc
Avanti <vitor.metzner@penseavanti.com.br>
2021-07-16 16:28:04 -0300

    render image , product,  schema

--- react-app-template-project-2/react/Shelf.tsx
+++ react-app-template-project-2/react/Shelf.tsx
@@ -18,11 +18,30 @@ const CSS_HANDLES = [
   'minimumButton',
   'maximumButton',
   'inputCount',
+  'valueShelf',
+  'valueOff',
+  'Discount',
+  'discountContainer',
+  'discountContent'
 ]
-const Shelf = () => {
+const valueOff = (value: number) => {
+  return value.toLocaleString('pt-BR', {
+    currency: 'BRL',
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  })
+}
+
+const Shelf = ({discount}: ShelfProps) => {
   const handles = useCssHandles(CSS_HANDLES)
   const [shelfWoman, setShelfWoman] = useState<IShelf[]>([])
-
+  const [counter, setCounter] = useState(0)
+  const incremeant = () =>{
+    setCounter((c) => c +1)
+  }
+  const decrement =() =>{
+    setCounter((c) => (!c ? c : c  -1 ))
+  }
   useEffect(() => {
     axios
       .get('/api/catalog_system/pub/products/search/woman')
@@ -32,30 +51,47 @@ const Shelf = () => {
   }, [])
   return (
     <div className={`${handles.shelfContainer}`}>
-      <SliderLayout itemsPerPage={{ desktop: 4, phone: 2 }}>
+      <SliderLayout itemsPerPage={{ desktop: 4, phone: 2 }} >
         {shelfWoman.map((item) => {
           return (
             <div className={`${handles.shelfImage}`}>
+              <div className={`${handles.discountContainer}`}>
+                <div className={`${handles.discountContent}`}>
+                      {discount}% OFF
+                </div>
+              </div>
               <div className={`${handles.renderImage}`}>
                 <img
                   src={item.items[0].images[0].imageUrl}
                   alt={item.items[0].name}
-                  width={160}
-                  height={160}
                 />
                 <div className={`${handles.subtitleShelf}`}>
                   <div className={`${handles.subtitleContent}`}>
                     <div>{item.items[0].name}</div>
                   </div>
+                  <div className={`${handles.valueShelf}`}>
+                    <div className={`${handles.valueOff}`}>
+                      R$
+                      {valueOff(
+                        item.items[0].sellers[0].commertialOffer.Price * 0.9
+                      )}
+                    </div>
+                    <div className={`${handles.Discount}`}>
+                      R$
+                      {valueOff(
+                        item.items[0].sellers[0].commertialOffer.Price
+                      )}
+                  </div>
+                  </div>
                   <div className={`${handles.countContainer}`}>
                     <div className={`${handles.minimumButton}`}>
-                      <Button />
+                      <Button  onClick={decrement}> - </Button>
                     </div>
                     <div className={`${handles.inputCount}`}>
-                      <Input />
+                      <Input type="text" readOnly value={counter}/>
                     </div>
                     <div className={`${handles.maximumButton}`}>
-                      <Button />
+                      <Button onClick={incremeant} > + </Button>
                     </div>
                   </div>
                   <div className={`${handles.buttonContainer}`}>
@@ -69,6 +105,23 @@ const Shelf = () => {
       </SliderLayout>
     </div>
   )
+}
+
+type ShelfProps = {
+  discount: number
+}
+Shelf.schema = {
+  title: 'Shelf',
+  description: 'Shelf Items',
+  type: 'object',
+  properties: {
+    discount: {
+      title: 'porcentagem',
+      description: 'number',
+      type: 'number',
+      default: '10',
+    },
+  },
 }
 
 export default Shelf
