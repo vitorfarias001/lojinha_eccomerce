4fd050bf55361f24c50b840047d10da8e113cbad
RodrigoContiRibeiro <rodrigo.ribeiro@penseavanti.com.br>
2021-07-19 17:47:21 -0300

    Fixed some style of the shelf, and limited the scope of the quantity stepper

--- react-app-template-project-2/react/Shelf.tsx
+++ react-app-template-project-2/react/Shelf.tsx
@@ -1,47 +1,22 @@
+// eslint-disable-next-line no-use-before-define
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useCssHandles } from 'vtex.css-handles'
-import { IShelf } from './typings/shelf'
 import { SliderLayout } from 'vtex.slider-layout'
-import { Button, Input } from 'vtex.styleguide'
 
-const CSS_HANDLES = [
-  'shelfTwo',
-  'shelfContainer',
-  'titleShelf',
-  'shelfImage',
-  'renderImage',
-  'subtitleShelf',
-  'subtitleContent',
-  'buttonContainer',
-  'countContainer',
-  'minimumButton',
-  'maximumButton',
-  'inputCount',
-  'valueShelf',
-  'valueOff',
-  'Discount',
-  'discountContainer',
-  'discountContent',
-]
-const valueOff = (value: number) => {
-  return value.toLocaleString('pt-BR', {
-    currency: 'BRL',
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2,
-  })
+import { IShelf } from './typings/shelf'
+import ShelfItem from './components/shelf/ShelfItem'
+
+const CSS_HANDLES = ['shelfContainer']
+
+type ShelfProps = {
+  discount: number
 }
 
 const Shelf = ({ discount }: ShelfProps) => {
   const handles = useCssHandles(CSS_HANDLES)
   const [shelfWoman, setShelfWoman] = useState<IShelf[]>([])
-  const [counter, setCounter] = useState(0)
-  const incremeant = () => {
-    setCounter((c) => c + 1)
-  }
-  const decrement = () => {
-    setCounter((c) => (!c ? c : c - 1))
-  }
+
   useEffect(() => {
     axios
       .get('/api/catalog_system/pub/products/search/woman')
@@ -49,55 +24,13 @@ const Shelf = ({ discount }: ShelfProps) => {
         setShelfWoman(response.data)
       })
   }, [])
+
   return (
     <div className={`${handles.shelfContainer}`}>
       <SliderLayout itemsPerPage={{ desktop: 4, phone: 2 }}>
         {shelfWoman.map((item) => {
           return (
-            <div className={`${handles.shelfImage}`}>
-              <div className={`${handles.discountContainer}`}>
-                <div className={`${handles.discountContent}`}>
-                  {discount}% OFF
-                </div>
-              </div>
-              <div className={`${handles.renderImage}`}>
-                <img
-                  src={item.items[0].images[0].imageUrl}
-                  alt={item.items[0].name}
-                />
-                <div className={`${handles.subtitleShelf}`}>
-                  <div className={`${handles.subtitleContent}`}>
-                    <div>{item.items[0].name}</div>
-                  </div>
-                  <div className={`${handles.valueShelf}`}>
-                    <div className={`${handles.Discount}`}>
-                      R$
-                      {valueOff(item.items[0].sellers[0].commertialOffer.Price)}
-                    </div>
-                    <div className={`${handles.valueOff}`}>
-                      R$
-                      {valueOff(
-                        item.items[0].sellers[0].commertialOffer.Price * 0.9
-                      )}
-                    </div>
-                  </div>
-                  <div className={`${handles.countContainer}`}>
-                    <div className={`${handles.minimumButton}`}>
-                      <Button onClick={decrement}> - </Button>
-                    </div>
-                    <div className={`${handles.inputCount}`}>
-                      <Input type="text" readOnly value={counter} />
-                    </div>
-                    <div className={`${handles.maximumButton}`}>
-                      <Button onClick={incremeant}> + </Button>
-                    </div>
-                  </div>
-                  <div className={`${handles.buttonContainer}`}>
-                    <Button>Adicionar</Button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ShelfItem item={item} discount={discount} key={item.productId} />
           )
         })}
       </SliderLayout>
@@ -105,9 +38,6 @@ const Shelf = ({ discount }: ShelfProps) => {
   )
 }
 
-type ShelfProps = {
-  discount: number
-}
 Shelf.schema = {
   title: 'Shelf',
   description: 'Shelf Items',
---
+++ react-app-template-project-2/react/components/shelf/ShelfItem.tsx
@@ -0,0 +1,98 @@
+// eslint-disable-next-line no-use-before-define
+import React, { useState } from 'react'
+import { Button, Input } from 'vtex.styleguide'
+import { useCssHandles } from 'vtex.css-handles'
+
+import { IShelf } from '../../typings/shelf'
+
+interface ShelfItemProps {
+  item: IShelf
+  discount: number
+}
+
+const CSS_HANDLES = [
+  'titleShelf',
+  'shelfItem',
+  'itemContent',
+  'subtitleShelf',
+  'subtitleContent',
+  'buttonContainer',
+  'countContainer',
+  'minimumButton',
+  'maximumButton',
+  'inputCount',
+  'productImage',
+  'valueShelf',
+  'valueOff',
+  'Discount',
+  'discountContainer',
+  'discountContent',
+] as const
+
+const valueOff = (value: number) => {
+  return value.toLocaleString('pt-BR', {
+    currency: 'BRL',
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  })
+}
+
+const ShelfItem = ({ item, discount }: ShelfItemProps) => {
+  const handles = useCssHandles(CSS_HANDLES)
+
+  const [counter, setCounter] = useState(0)
+
+  const incremeant = () => {
+    setCounter((c) => c + 1)
+  }
+
+  const decrement = () => {
+    setCounter((c) => (!c ? c : c - 1))
+  }
+
+  return (
+    <div className={`${handles.shelfItem}`} key={item.productId}>
+      <div className={`${handles.itemContent}`}>
+        <div className={`${handles.discountContainer}`}>
+          <div className={`${handles.discountContent}`}>{discount}% OFF</div>
+        </div>
+        <img
+          src={item.items[0].images[0].imageUrl}
+          alt={item.items[0].name}
+          className={handles.productImage}
+        />
+        <div className={`${handles.subtitleShelf}`}>
+          <div className={`${handles.subtitleContent}`}>
+            <div>{item.items[0].name}</div>
+          </div>
+          <div className={`${handles.valueShelf}`}>
+            <div className={`${handles.Discount}`}>
+              R$
+              {valueOff(item.items[0].sellers[0].commertialOffer.Price)}
+            </div>
+            <div className={`${handles.valueOff}`}>
+              R$
+              {valueOff(item.items[0].sellers[0].commertialOffer.Price * 0.9)}
+            </div>
+          </div>
+          <div className={`${handles.countContainer}`}>
+            <div className={`${handles.minimumButton}`}>
+              <Button onClick={decrement}>-</Button>
+            </div>
+            <div className={`${handles.inputCount}`}>
+              <Input type="text" readOnly value={counter} />
+            </div>
+            <div className={`${handles.maximumButton}`}>
+              <Button onClick={incremeant}>+</Button>
+            </div>
+          </div>
+          <div className={`${handles.buttonContainer}`}>
+            <Button>Adicionar</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShelfItem
